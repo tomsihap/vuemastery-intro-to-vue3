07e5f71e54b61762140367c9d36d61fe631cb59d
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2021-03-01 12:36:38 +0100

    list renderin

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ const app = Vue.createApp({
             moreLink: 'http://www.example.com',
             inStock: true,
             inventory: 9,
-            onSale: true
+            onSale: true,
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 2234,
+                    color: 'green'
+                },
+                {
+                    id: 2235,
+                    color: 'blue'
+                }
+            ],
+            sizes: [38, 39, 40, 41, 42, 43, 44]
         }
     }
 })
