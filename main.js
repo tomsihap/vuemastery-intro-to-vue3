1c51f4dc6baba967ed0e0ed96fb26c0f3aad62b9
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2021-03-01 13:15:22 +0100

    computed properties

--- main.js
+++ main.js
@@ -2,23 +2,25 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Socks',
+            brand: 'Hello',
             description: 'Good stuff',
-            image: './assets/images/socks_green.jpg',
+            selectedVariant: 0,
             moreLink: 'http://www.example.com',
-            inStock: true,
-            inventory: 9,
-            onSale: true,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 {
                     id: 2234,
                     color: 'green',
-                    image: './assets/images/socks_green.jpg'
+                    image: './assets/images/socks_green.jpg',
+                    quantity: 50,
+                    onSale: false
                 },
                 {
                     id: 2235,
                     color: 'blue',
-                    image: './assets/images/socks_blue.jpg'
+                    image: './assets/images/socks_blue.jpg',
+                    quantity: 0,
+                    onSale: true
                 }
             ],
             sizes: [38, 39, 40, 41, 42, 43, 44],
@@ -27,10 +29,26 @@ const app = Vue.createApp({
     },
     methods: {
         addToCart() {
-            this.cart++
+            this.cart++;
+            this.variants[this.selectedVariant].quantity--;
         },
-        updateImage(variantImage) {
-            this.image = variantImage
+        updateVariant(index) {
+            this.selectedVariant = index;
         }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVariant].image
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].quantity
+        },
+        onSale() {
+            return this.variants[this.selectedVariant].onSale
+        }
+
     }
 })
